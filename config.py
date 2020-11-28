69d331f0cf41612950ded229dc90ad662058c432
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-11-28 18:27:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1437096917:AAHd0B5SK1sO1iZi-2r3e3XCVJuSZJyjQHM"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Eliza updater"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/PerU-MoNsteR/GitGram"
